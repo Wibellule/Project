553b347eb7f512ed013c7340a31127d60f970c39
gyome34 <gyome34@hotmail.com>
2013-05-10 14:23:26 +0200

    Modification categories_controller.php/backoffice_index()
    - Surcharge de la fonction backoffice_index();
    - Modification affichage, seules les branches et les feuilles sont dispo
    - Ajout des types dans la table categories pour filtrer

--- controllers/categories_controller.php
+++ controllers/categories_controller.php
@@ -42,9 +42,47 @@ class CategoriesController extends AppController{
 	}
 	
 	function backoffice_index(){
-		parent::backoffice_index();
+		// parent::backoffice_index();
+		
+		$this->loadModel('Categorie');
+		$d['elementsPerPage'] = 50;
+		$conditions = array(
+			'conditions' => 'type != 3',
+			'fields' => array('id', 'name', 'lft', 'rgt', 'level', 'online', 'type'), 
+			'order' => 'lft'
+		);
+		$d['categories'] = $this->Categorie->find($conditions);		
+		$d['nbElem'] = $this->Categorie->findCount('type != 3');
+		$d['nbPages'] = ceil($d['nbElem'] / $d['elementsPerPage']);
+		$this->set($d);
+		
 		// pr($this->Categorie->getTree());
 		// die();
+	}
+	
+	function backoffice_add($id = null){
+
+		$this->loadModel('Categorie');
+		if($this->request->data){
+			
+			//////////////////////////////
+			// Code validation François //
+			//////////////////////////////
+			if($this->Categorie->validates($this->request->data)){
+				$this->request->data['type'] = 1;
+				$this->Categorie->save($this->request->data);
+				Session::setFlash('Element ajouté avec succes','success');
+				$this->redirect('/adm/categories/index');
+			}else{
+				$errors = $this->$modelName->errors;
+				$message = "Erreur dans le formulaire<br/><br/>";
+				foreach($errors as $k => $v){
+					$message .= $v."<br />";
+				}
+				Session::setFlash($message,'error');
+			}			
+			//////////////////////////////
+		}
 	}
 	
 	/**
