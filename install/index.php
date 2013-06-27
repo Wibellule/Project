539812c3ba5a2924d4ff353a2bd1a8b9d829513e
wibellule <gyome34@hotmail.com>
2013-06-27 13:15:29 +0200

    Mise à jour [install]
    - Ajout de la vérification pour l'import des tables, passe à l'étape
    suivante si ok sinon affiche le message d'erreur de la fonction init_db

--- install/index.php
+++ install/index.php
@@ -14,7 +14,7 @@ $steps = array(
 	'connect'			=> '- Test de la connexion',
 	'database' 			=> '- Configuration de la base de données ',
 	'import_tables'		=> '- Import des tables de la base de données ',
-	'database_datas'	=> '- Import des données ',
+	'import_datas'	=> '- Import des données ',
 	'final'				=> '- Récapitulatif de l\'installation '
 );
 
@@ -73,7 +73,7 @@ else { $step = $_GET['step']; }
             <ul class="nav nav-list">
               <li class="nav-header">Étapes</li>
               <li <?php echo $step == 'accueil' ? 'class="active"' : '';?>><a>Accueil</a></li>
-              <li <?php echo in_array($step, array('database', 'connect', 'import_tables', 'database_datas')) ? 'class="active"' : ''; ?>><a>Base de données</a></li>
+              <li <?php echo in_array($step, array('database', 'connect', 'import_tables', 'import_datas')) ? 'class="active"' : ''; ?>><a>Base de données</a></li>
               <li <?php echo $step == 'final' ? 'class="active"' : '';?>><a>Récapitulatif</a></li>
             </ul>
           </div><!--/.well -->
--- install/pages/import_tables.php
+++ install/pages/import_tables.php
@@ -22,55 +22,63 @@ $init_db_tables = init_db($conf['host'], $conf['database'], $conf['login'], $con
 // pr($init_db_tables);
 ?>
 <h2>IMPORT DES TABLES DE LA BASE DE DONNEES</h2>
-<table class="table table-striped">
-	<tr>
-		<th id="MatrixItems">&nbsp;</th>
-		<th class="tablecol">Session</th>
-		<th class="tablecol">Effectué</th>
-		<th class="tablecol">Total</th>
-	</tr>
-	<tr>
-		<td class="tableid first">Lignes</td>
-		<td class="odd"><?php echo $init_db_tables['datas']['lines_this']; ?></td>
-		<td class="even"><?php echo $init_db_tables['datas']['lines_done']; ?></td>
-		<td class="odd"><?php echo $init_db_tables['datas']['lines_tota']; ?></td>
-	</tr>
-	<tr>
-		<td class="tableid first">Requêtes</td>
-		<td class="odd"><?php echo $init_db_tables['datas']['queries_this']; ?></td>
-		<td class="even"><?php echo $init_db_tables['datas']['queries_done']; ?></td>
-		<td class="odd"><?php echo $init_db_tables['datas']['queries_tota']; ?></td>
-	</tr>
-	<tr>
-		<td class="tableid first">Volume en Bytes</td>
-		<td class="odd"><?php echo $init_db_tables['datas']['bytes_this']; ?></td>
-		<td class="even"><?php echo $init_db_tables['datas']['bytes_done']; ?></td>
-		<td class="odd"><?php echo $init_db_tables['datas']['bytes_tota']; ?></td>
-	</tr>
-	<tr>
-		<td class="tableid first">Volume en KB</td>
-		<td class="odd"><?php echo $init_db_tables['datas']['kbytes_this']; ?></td>
-		<td class="even"><?php echo $init_db_tables['datas']['kbytes_done']; ?></td>
-		<td class="odd"><?php echo $init_db_tables['datas']['kbytes_tota']; ?></td>
-	</tr>
-	<tr>
-		<td class="tableid first">Volume en MB</td>
-		<td class="odd"><?php echo $init_db_tables['datas']['mbytes_this']; ?></td>
-		<td class="even"><?php echo $init_db_tables['datas']['mbytes_done']; ?></td>
-		<td class="odd"><?php echo $init_db_tables['datas']['mbytes_tota']; ?></td>
-	</tr>
-	<tr>
-		<td class="tableid first">%</td>
-		<td class="odd"><?php echo $init_db_tables['datas']['pct_this']; ?></td>
-		<td class="even"><?php echo $init_db_tables['datas']['pct_done']; ?></td>
-		<td class="odd"><?php echo $init_db_tables['datas']['pct_tota']; ?></td>
-	</tr>
-	<tr>
-		<td class="tableid first">% progression</td>
-		<td class="odd" colspan="4"><?php echo $init_db_tables['datas']['pct_bar']; ?></td>
-	</tr>
-</table>
-<form action="index.php?step=import_datas" method="post">
-	<input type="hidden" name="section" value="<?php echo $section; ?>" />
-	<button class="btn btn-large btn-primary" type="submit">Importer les tables</button></br>
-</form>
+	<?php if($init_db_tables['result']) { ?>
+	<table class="table table-striped">
+		<tr>
+			<th id="MatrixItems">&nbsp;</th>
+			<th class="tablecol">Session</th>
+			<th class="tablecol">Effectué</th>
+			<th class="tablecol">Total</th>
+		</tr>
+		<tr>
+			<td class="tableid first">Lignes</td>
+			<td class="odd"><?php echo $init_db_tables['datas']['lines_this']; ?></td>
+			<td class="even"><?php echo $init_db_tables['datas']['lines_done']; ?></td>
+			<td class="odd"><?php echo $init_db_tables['datas']['lines_tota']; ?></td>
+		</tr>
+		<tr>
+			<td class="tableid first">Requêtes</td>
+			<td class="odd"><?php echo $init_db_tables['datas']['queries_this']; ?></td>
+			<td class="even"><?php echo $init_db_tables['datas']['queries_done']; ?></td>
+			<td class="odd"><?php echo $init_db_tables['datas']['queries_tota']; ?></td>
+		</tr>
+		<tr>
+			<td class="tableid first">Volume en Bytes</td>
+			<td class="odd"><?php echo $init_db_tables['datas']['bytes_this']; ?></td>
+			<td class="even"><?php echo $init_db_tables['datas']['bytes_done']; ?></td>
+			<td class="odd"><?php echo $init_db_tables['datas']['bytes_tota']; ?></td>
+		</tr>
+		<tr>
+			<td class="tableid first">Volume en KB</td>
+			<td class="odd"><?php echo $init_db_tables['datas']['kbytes_this']; ?></td>
+			<td class="even"><?php echo $init_db_tables['datas']['kbytes_done']; ?></td>
+			<td class="odd"><?php echo $init_db_tables['datas']['kbytes_tota']; ?></td>
+		</tr>
+		<tr>
+			<td class="tableid first">Volume en MB</td>
+			<td class="odd"><?php echo $init_db_tables['datas']['mbytes_this']; ?></td>
+			<td class="even"><?php echo $init_db_tables['datas']['mbytes_done']; ?></td>
+			<td class="odd"><?php echo $init_db_tables['datas']['mbytes_tota']; ?></td>
+		</tr>
+		<tr>
+			<td class="tableid first">%</td>
+			<td class="odd"><?php echo $init_db_tables['datas']['pct_this']; ?></td>
+			<td class="even"><?php echo $init_db_tables['datas']['pct_done']; ?></td>
+			<td class="odd"><?php echo $init_db_tables['datas']['pct_tota']; ?></td>
+		</tr>
+		<tr>
+			<td class="tableid first">% progression</td>
+			<td class="odd" colspan="4"><?php echo $init_db_tables['datas']['pct_bar']; ?></td>
+		</tr>
+	</table>
+	<form action="index.php?step=import_datas" method="post">
+		<input type="hidden" name="section" value="<?php echo $section; ?>" />
+		<button class="btn btn-large btn-primary" type="submit">Importer les données</button></br>
+	</form>
+<?php 
+	}else{
+		foreach($init_db_tables['datas'] as $k => $v){
+			echo '<div class="alert alert-error">'.$v.'</div>';
+		}
+	} 
+?>
