b7b1ed180d9611866c0634721f4568ea406d181b
wibellule <gyome34@hotmail.com>
2013-07-04 15:37:50 +0200

    Mise à jour [view][layout][backoffice]
    - Bar des menus [incomplète]

--- views/layout/backoffice.php
+++ views/layout/backoffice.php
@@ -18,8 +18,102 @@ $nb_messages = $messages->findCount($conditions_messages);
 		<?php if(isset($description_for_layout) && !empty($description_for_layout)) { ?>
 			<meta name="description" content="<?php echo $description_for_layout; ?>">
 		<?php } ?>
+		<link href="<?php echo Router::webroot('css/backoffice/css/_bootstrap.css');?>" rel="stylesheet" type="text/css" />
+		<!--<link href="<?php //echo Router::webroot('css/backoffice/css/bootstrap-docs.css');?>" rel="stylesheet" type="text/css" />
+		<link href="<?php //echo Router::webroot('css/backoffice/css/bootstrap-responsive.css');?>" rel="stylesheet" type="text/css" />
+		<link href="<?php //echo Router::webroot('css/backoffice/css/prettify.css');?>" rel="stylesheet" type="text/css" />-->
+		<link href="<?php echo Router::webroot('css/backoffice/css/_flat-ui.css');?>" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
+	
+	<div class="container">
+	
+		<div class="demo-headline">
+			<h4 class="demo-logo">Flat UI<small>Free Web User Interface Kit</small></h4>
+		</div> <!-- /demo-headline -->
+		
+		<div class="row demo-row">
+      
+          <div class="navbar navbar-inverse">
+            <div class="navbar-inner">
+              <div class="container">
+                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <div class="nav-collapse collapse">
+                  <ul class="nav">
+                    <li class="active">
+                      <a href="<?php echo Router::url('adm');?>">Accueil</a>
+                    </li>
+					<li>
+                      <a href="#">Catégories
+                        <span class="navbar-unread">1</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="<?php echo Router::url('adm/posts/index');?>">
+                        Articles
+                        <span class="navbar-unread">1</span>
+                      </a>
+                      <ul>
+                        <li><a href="<?php echo Router::url('adm/postscomments/index');?>">Commentaires</a></li>
+                        <li><a href="#">Element One</a></li>
+                        <li><a href="#">Element One</a></li>
+                        <li>
+                          <a href="#">Sub menu</a>
+                          <ul>
+                            <li><a href="#">Element One</a></li>
+                            <li><a href="#">Element Two</a></li>
+                            <li><a href="#">Element Three</a></li>
+                          </ul> <!-- /Sub menu -->
+                        </li>
+                        <li><a href="#">Element Three</a></li>
+                      </ul> <!-- /Sub menu -->
+                    </li>
+					<li class="active">
+                      <a href="#">
+                        Messages
+                        <span class="navbar-unread">1</span>
+                      </a>
+                      <ul>
+                        <li><a href="#">Element One</a></li>
+                        <li>
+                          <a href="#">Sub menu</a>
+                          <ul>
+                            <li><a href="#">Element One</a></li>
+                            <li><a href="#">Element Two</a></li>
+                            <li><a href="#">Element Three</a></li>
+                          </ul> <!-- /Sub menu -->
+                        </li>
+                        <li><a href="#">Element Three</a></li>
+                      </ul> <!-- /Sub menu -->
+                    </li>
+					<li>
+                      <a href="#">Projets
+                        <span class="navbar-unread">1</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="#">Sliders
+                        <span class="navbar-unread">1</span>
+                      </a>
+                    </li>
+					<li>
+                      <a href="#">Déconnexion
+                        <span class="navbar-unread">1</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div><!--/.nav-collapse -->
+              </div>
+			</div>
+		</div>
+	</div>
+	
+	
+	
 		<ul>
 		  <li><a href="<?php echo Router::url('adm');?>" title=""><i class="icon-home"></i>&nbsp;Accueil</a></li>
 		  <li><a href="<?php echo Router::url('adm/posts/index');?>" title=""><i class="icon-tag"></i>&nbsp;Articles</a></li>
@@ -33,5 +127,18 @@ $nb_messages = $messages->findCount($conditions_messages);
 		  <li><a href="<?php echo Router::url('users/logout');?>" title=""><i class="icon-ban-circle"></i>&nbsp;Déconnexion</a></li>
 		</ul>
 		<?php echo $content_for_layout;?>
+		
+		</div>
+		
+		<script src="<?php echo Router::webroot('css/backoffice/js/jquery-1.8.2.min.js');?>"></script>
+		<script src="<?php echo Router::webroot('css/backoffice/js/jquery-ui-1.10.0.custom.min.js');?>"></script>
+		<script src="<?php echo Router::webroot('css/backoffice/js/jquery.dropkick-1.0.0.js');?>"></script>
+		<script src="<?php echo Router::webroot('css/backoffice/js/custom_checkbox_and_radio.js');?>"></script>
+		<script src="<?php echo Router::webroot('css/backoffice/js/custom_radio.js');?>"></script>
+		<script src="<?php echo Router::webroot('css/backoffice/js/jquery.tagsinput.js');?>"></script>
+		<script src="<?php echo Router::webroot('css/backoffice/js/bootstrap-tooltip.js');?>"></script>
+		<script src="<?php echo Router::webroot('css/backoffice/js/jquery.placeholder.js');?>"></script>
+		<script src="http://vjs.zencdn.net/c/video.js"></script>
+		<script src="<?php echo Router::webroot('css/backoffice/js/application.js');?>"></script>
 	</body>	
 </html>
